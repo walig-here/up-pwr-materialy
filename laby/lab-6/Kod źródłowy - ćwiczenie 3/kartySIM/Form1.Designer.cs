f44e20ee41d7d9b22cdf1caba9f5cf346dab9239
walig-here <daw.walig@gmail.com>
2024-01-31 21:36:06 +0100

    Sprawozdanie z lab 6

---
+++ laby/lab-6/Kod źródłowy - ćwiczenie 3/kartySIM/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace kartySIM
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.readerList = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.connectButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.sendCommandButton = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.responseBoxHex = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.responseBoxAscii = new System.Windows.Forms.TextBox();
+            this.commandList = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // readerList
+            // 
+            this.readerList.FormattingEnabled = true;
+            this.readerList.Location = new System.Drawing.Point(82, 9);
+            this.readerList.Name = "readerList";
+            this.readerList.Size = new System.Drawing.Size(519, 28);
+            this.readerList.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Czytnik:";
+            // 
+            // connectButton
+            // 
+            this.connectButton.Location = new System.Drawing.Point(607, 6);
+            this.connectButton.Name = "connectButton";
+            this.connectButton.Size = new System.Drawing.Size(181, 32);
+            this.connectButton.TabIndex = 2;
+            this.connectButton.Text = "Połącz z kartą";
+            this.connectButton.UseVisualStyleBackColor = true;
+            this.connectButton.Click += new System.EventHandler(this.connectButton_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Komenda";
+            // 
+            // sendCommandButton
+            // 
+            this.sendCommandButton.Location = new System.Drawing.Point(607, 49);
+            this.sendCommandButton.Name = "sendCommandButton";
+            this.sendCommandButton.Size = new System.Drawing.Size(181, 33);
+            this.sendCommandButton.TabIndex = 5;
+            this.sendCommandButton.Text = "Wyślij";
+            this.sendCommandButton.UseVisualStyleBackColor = true;
+            this.sendCommandButton.Click += new System.EventHandler(this.sendCommandButton_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 100);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(127, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Odpowiedź (hex)";
+            // 
+            // responseBoxHex
+            // 
+            this.responseBoxHex.Location = new System.Drawing.Point(17, 136);
+            this.responseBoxHex.Name = "responseBoxHex";
+            this.responseBoxHex.Size = new System.Drawing.Size(770, 26);
+            this.responseBoxHex.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 190);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(145, 20);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Odpowiedź (ASCII)";
+            // 
+            // responseBoxAscii
+            // 
+            this.responseBoxAscii.Location = new System.Drawing.Point(21, 224);
+            this.responseBoxAscii.Name = "responseBoxAscii";
+            this.responseBoxAscii.Size = new System.Drawing.Size(765, 26);
+            this.responseBoxAscii.TabIndex = 9;
+            // 
+            // commandList
+            // 
+            this.commandList.FormattingEnabled = true;
+            this.commandList.Items.AddRange(new object[] {
+            "select file telecom",
+            "select file mf",
+            "select file adn",
+            "get response",
+            "read record"});
+            this.commandList.Location = new System.Drawing.Point(96, 52);
+            this.commandList.Name = "commandList";
+            this.commandList.Size = new System.Drawing.Size(505, 28);
+            this.commandList.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 784);
+            this.Controls.Add(this.commandList);
+            this.Controls.Add(this.responseBoxAscii);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.responseBoxHex);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.sendCommandButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.connectButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.readerList);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox readerList;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button connectButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button sendCommandButton;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox responseBoxHex;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox responseBoxAscii;
+        private System.Windows.Forms.ComboBox commandList;
+    }
+}
+
---
+++ laby/lab-6/Kod źródłowy - ćwiczenie 3/kartySIM/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using PCSC;
+
+namespace kartySIM
+{
+    public partial class Form1 : Form
+    {
+        SCardReader reader;
+        SCardContext context = new SCardContext();
+
+        public Form1()
+        {
+            InitializeComponent();
+            sendCommandButton.Enabled = false;
+            if (!getReaders())
+                System.Environment.Exit(0);
+        }
+
+        bool getReaders()
+        {
+            context.Establish(SCardScope.System);
+            string[] availableReaders = context.GetReaders();
+            if(availableReaders.Length == 0)
+            {
+                MessageBox.Show("Brak czytnika");
+                return false;
+            }
+            foreach (var availableReader in availableReaders)
+            {
+                readerList.Items.Add(availableReader);
+            }
+
+            readerList.SelectedIndex = 0;
+            return true;
+        }
+
+        void parseAtr(byte[] atr)
+        {
+            string atrStr = "";
+            if (atr[0] == 0x3b)
+                atrStr += "Direct Convention";
+            else if (atr[0] == 0x3f)
+                atrStr += "Inverse Convention";
+            atrStr += ", ";
+
+            atrStr += "Protokół T" + (atr[3] % 16).ToString() + ", ";
+
+            int historical_bytes = atr[1] % 16;
+            atrStr += (atr[1]%16).ToString() + " znaków historycznych: ";
+            for (int i = 7; i < 7 + historical_bytes; i++)
+                atrStr += atr[i] + " ";
+
+            responseBoxAscii.Text = atrStr;
+        }
+
+        private void connectButton_Click(object sender, EventArgs e)
+        {
+            reader = new SCardReader(context);           
+            SCardError error = reader.Connect(readerList.SelectedItem.ToString(), SCardShareMode.Shared, SCardProtocol.T0 | SCardProtocol.T1);
+          
+            if (!checkCardError(error))
+                return;
+            
+            if (reader.ActiveProtocol != SCardProtocol.T0 && reader.ActiveProtocol != SCardProtocol.T1)
+            {
+                MessageBox.Show("Aktywny protokół nie jest odbługiwany!");
+                sendCommandButton.Enabled = false;
+            }
+            else
+            {
+                MessageBox.Show("Połączono z kartą");
+                sendCommandButton.Enabled = true;
+                byte[] buffer;
+                reader.GetAttrib(SCardAttribute.AnswerToResetString, out buffer);
+                printToRespondeBox(buffer);
+                parseAtr(buffer);
+            }
+        }
+
+        void sendCommand()
+        {
+            string commandString = commandList.SelectedItem.ToString();
+            byte[] command = new byte[] { };
+            byte[] response = new byte[256];
+            switch (commandString)
+            {
+                case "select file telecom":
+                    command = new byte[] { 0xA0, 0xA4, 0x00, 0x00, 0x02, 0x7F, 0x10 };
+                    break;
+                case "select file adn":
+                    command = new byte[] { 0xA0, 0xA4, 0x00, 0x00, 0x02, 0x6f, 0x3a };
+                    break;
+                case "select file mf":
+                    command = new byte[] { 0xA0, 0xA4, 0x00, 0x00, 0x02, 0x3f, 0x00 };
+                    break;
+                case "read record":
+                    command = new byte[] { 0xA0, 0xB2, 0x00, 0x04, 0x1C};
+                    break;
+                case "get response":
+                    command = new byte[] { 0xa0, 0xc0, 0x00, 0x00, 0xFF};
+                    break;
+                default:
+                    MessageBox.Show("Nieprawidłowa komenda");
+                    return;
+            }
+            SCardError error = reader.Transmit(command, ref response);
+            if (!checkCardError(error))
+                return;
+            printToRespondeBox(response);
+        }
+
+        bool checkCardError(SCardError error)
+        {
+            if(error != SCardError.Success)
+            {
+                MessageBox.Show("Wystąpił błąd karty: " + error.ToString());
+                return false;
+            }
+            return true;
+        }
+
+        private void sendCommandButton_Click(object sender, EventArgs e)
+        {
+            if (commandList.SelectedItem != null)
+                sendCommand();
+            else
+                MessageBox.Show("Pole komendy jest puste");
+        }
+
+        void printToRespondeBox(byte[] response)
+        {
+            string answerStrHex = BitConverter.ToString(response).Replace("-", " ");
+            responseBoxHex.Text = answerStrHex;
+            
+            string answerStrAscii = "";
+            switch (response[0])
+            {
+                case 0x9f:
+                    answerStrAscii = "Komenda wykonała się poprawnie. Pozostało " + response[1] + " bajtów odpowiedzi.";
+                    break;
+                default:
+                    answerStrAscii = System.Text.Encoding.ASCII.GetString(response);
+                    break;
+            }
+            responseBoxAscii.Text = answerStrAscii;
+        }
+    }
+}
